d609508ab434ac7f0aa58d831a27982d3b38e5cc
kjcollins <kathrynjcollins@gmail.com>
2018-02-03 15:12:31 -0500

    restructuring

--- messaging.py
+++ messaging.py
@@ -6,43 +6,29 @@ from twilio.rest import Client
 from twilio import twiml
 from nltk.corpus import gutenberg
 
-
+def parseUserMessage(msg, response):
+	if "hi" == msg:
+		response.message('Holla!')
+	elif "hello" == msg:
+		response.message("Stand, ho!")
+	else:
+		response.message(inputHandler(msg))
 
 app = Flask(__name__)
 @app.route('/sms', methods=['GET', 'POST'])
 
 def sms():
-	msg = request.form.get('Body').lower().strip() #gets incoming message
-	# if msg == "hi": #based on incoming message, send different message
+	msg = request.form.get('Body').lower().strip() # gets incoming message
+	
 	resp = MessagingResponse()
-	if "hi" == msg:
-		resp.message('Holla!')# , to="+17162138995")
-	else:
-		resp = resp.message("Stand, ho!")
+	
+	# based on incoming message, send different message
+	to_respond = parseUserMessage(msg)
+
+	resp.message(to_respond)
+	
 	#return message: if you just want to send any message, return this
 	return str(resp)
 
 if __name__ == "__main__":
 	app.run(debug=True)
-
-
-
-
-
-
-# for num in client.api.account.messages.list('+16572206721'):
-# 	client.api.account.calls.create(
-# 		to=num,
-# 		from_='+16572206721',
-# 		url = 'http://demo.twilio.com/docs/classic.mp3'
-# 		)
-
-# Find these values at https://twilio.com/user/account
-# account_sid = 
-# auth_token = 
-
-# client = Client(account_sid, auth_token)
-# client.api.account.messages.create(
-#     to="",
-#     from_="",
-#     body="Hello there!")
